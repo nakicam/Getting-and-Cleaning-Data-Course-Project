d852e3675b38358c69c8576bb2c3feeae8ca747b
Naki Cam <Nakicam@gmail.com>
2016-06-17 14:02:41 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -26,17 +26,17 @@ data_train_subject <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 #One data set for X
 data_all_X <- rbind(data_train_X, data_test_X)
 
-#One data set for Y
+#dataset Y
 data_all_Y <- rbind(data_train_Y, data_test_Y)
 
-#One data set for Subject
+#dataset subject
 data_all_subject <- rbind(data_train_subject, data_test_subject)
 
 #2. Extract only the measurements on the mean and standard deviation for each measurement.
-#Load features.txt file
+#Load features.txt
 data_features <- read.table("./UCI HAR Dataset/features.txt")
 
-#Get list of mean and std columns
+#list of mean and std columns
 mean_std <- grep("-(mean|std)\\(\\)", data_features[, 2])
 
 #Subset the mean and std columns
@@ -46,7 +46,7 @@ data_all_X <- data_all_X[, mean_std]
 names(data_all_X) <- data_features[mean_std, 2]
 
 #3. Use descriptive activity names to name the activities in the data set
-#Load activity_labels.txt file
+#Load activity_labels.txt
 data_activities <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
 #Update correct activity name
@@ -57,10 +57,10 @@ names(data_all_Y) <- "activity"
 #4. labels the data set with descriptive variable names
 names(data_all_subject) <- "subject"
 
-# bind all the data in a single data set
+# cbind all the data in one dataset
 all_data <- cbind(data_all_X, data_all_Y, data_all_subject)
 
-#Update label of the data
+#Updates names of the data
 names(all_data)<-gsub("std()", "SD", names(all_data))
 names(all_data)<-gsub("mean()", "MEAN", names(all_data))
 names(all_data)<-gsub("^t", "time", names(all_data))
